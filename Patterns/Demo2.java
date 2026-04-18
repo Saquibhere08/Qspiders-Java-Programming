f7180d1dc55c555336b732ff51c1eb6d7823b5d7
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-04-18 18:02:19 +0530

    Add files via upload

---
+++ Patterns/Demo2.java
@@ -0,0 +1,22 @@
+package Pattern;
+
+public class Demo2 {
+public static void main(String[] args) {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                if(i== 0 || j == 4)
+                {
+                    System.out.print("^");
+                
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }    
+}
---
+++ Patterns/Demo8.java
@@ -0,0 +1,23 @@
+package Pattern;
+
+public class Demo8 {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                //if(i==0 || i==4 || j==0 || j==4 || i==2 || j==2)
+                if(i%2!=0 && j%2!=0)
+                {
+                    System.out.print("*  ");
+                
+                }
+                else
+                {
+                    System.out.print("@  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
