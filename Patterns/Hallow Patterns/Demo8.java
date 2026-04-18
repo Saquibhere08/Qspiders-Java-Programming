cbea9cf5c3b5e35b9f04a78babb78aae1c34534c
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-04-18 18:03:26 +0530

    Add files via upload

---
+++ Patterns/Hallow Patterns/Demo8.java
@@ -0,0 +1,22 @@
+package Pattern.HallowPattern;
+
+public class Demo8 {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                if(j==0 || j==4 || i==j)
+                {
+                    System.out.print("@ ");
+                
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
